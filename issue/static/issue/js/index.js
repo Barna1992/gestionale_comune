4c630ced39e8d15e5b29a6170404da54fd5ec071
Barna1992 <andrea.barnabo92@gmail.com>
2020-02-29 00:11:22 +0100

    Aggiungo tutti i files nei nuovi path

---
+++ issue/static/issue/js/index.js
@@ -0,0 +1,19 @@
+$(function() {
+    $('#id_expired_date').addClass('datepicker')
+    $( ".datepicker" ).datepicker({
+      changeMonth: true,
+      changeYear: true,
+      yearRange: "1900:2012",
+      // You can put more options here.
+
+    });
+});
+
+$('input[type=checkbox]').click( (i) => {
+    if ( $(i.currentTarget).prop('checked') ) {
+        $(i.currentTarget).parent().removeClass('button-checkbox').addClass('button-checkbox-checked');
+    }
+    else {
+        $(i.currentTarget).parent().removeClass('button-checkbox-checked').addClass('button-checkbox');
+    }
+})
